a5bff0ab212eb6788825218bc5f7bc23669b99f0
pkomolkoe <p.komolko@gmail.com>
2017-09-12 10:15:34 +0200

    zadanie 12.4 zadanie 12.5

---
+++ zadanie12.6/scripts.js
@@ -0,0 +1,20 @@
+var url = 'https://restcountries.eu/rest/v1/name/';
+var countriesList = $('#countries');
+
+$('#search').click(searchCountries);
+
+
+
+function searchCountries() {
+ 	var countryName = $('#country-name').val();
+	if(!countryName.length) countryName = 'Poland';
+	$.ajax({
+  		url: url + countryName,
+  		method: 'GET',
+  		success: showCountriesList
+  	});
+};
+
+function showCountriesList(resp) {
+  countriesList.empty();
+}
